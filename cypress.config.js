86830598e17088809b807d182d05f989e7db93f8
Walmyr <wlsf82@gmail.com>
2023-09-16 03:48:53 +0200

    Improve Cypress config readability

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('cypress')
 
+const cyPostgres = require('cypress-postgres-10v-compatibility')
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {
-        dbQuery: query => require('cypress-postgres-10v-compatibility')(
+        dbQuery: query => cyPostgres(
           query.query,
           query.connection
         )
